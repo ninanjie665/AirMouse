21769145db5c1b487e2dec159e6fcd22f93ae042
拟南芥 <3405075960@qq.com>
2026-04-08 10:52:36 +0800

    frist commit

---
+++ Core/Mycode/mpu6050.c
@@ -0,0 +1,55 @@
+#include "mpu6050.h"
+
+#include <stdio.h>
+
+float gyro_sensitivity = 16.4f;
+
+void MPU6050_WriteReg(uint8_t address,uint8_t data) {
+    uint8_t buf[2];
+
+    buf[0] = address;
+    buf[1] = data;
+
+    HAL_I2C_Master_Transmit(&hi2c1,MPU6050_ADDR,buf,2,10);
+}
+
+
+void MPU6050_Init(void) {
+    MPU6050_WriteReg(MPU6050_PWR_MGMT_1, 0x01);
+    HAL_Delay(10);
+    MPU6050_WriteReg(MPU6050_PWR_MGMT_2, 0x00);
+    HAL_Delay(10);
+    MPU6050_WriteReg(MPU6050_SMPLRT_DIV, 0x09);
+    HAL_Delay(10);
+    MPU6050_WriteReg(MPU6050_CONFIG, 0x03);
+    HAL_Delay(10);
+    MPU6050_WriteReg(MPU6050_GYRO_CONFIG, 0x18);
+    HAL_Delay(10);
+    MPU6050_WriteReg(MPU6050_ACCEL_CONFIG, 0x18);
+    HAL_Delay(10);
+}
+void MPU6050_GetGyroDPS(float *wx, float *wy, float *wz) {
+    uint8_t buf[6];
+
+    HAL_I2C_Mem_Read(&hi2c1,MPU6050_ADDR,MPU6050_GYRO_XOUT_H,
+        I2C_MEMADD_SIZE_8BIT,buf,6,10);
+
+    int16_t raw_x = (int16_t)((buf[0] << 8) | buf[1]); //x
+    int16_t raw_y = (int16_t)((buf[2] << 8) | buf[3]); //y
+    int16_t raw_z = (int16_t)((buf[4] << 8) | buf[5]); //z
+
+    float x_dps = raw_x / gyro_sensitivity;
+    float y_dps = raw_y / gyro_sensitivity;
+    float z_dps = raw_z / gyro_sensitivity;
+
+    static float x_filt = 0, y_filt = 0, z_filt = 0;
+    float alpha = 0.2f;
+
+    x_filt = x_dps * alpha + (1 - alpha) * x_filt;
+    y_filt = y_dps * alpha + (1 - alpha) * y_filt;
+    z_filt = z_dps * alpha + (1 - alpha) * z_filt;
+
+    *wx = x_filt;
+    *wy = y_filt;
+    *wz = z_filt;
+}
---
+++ Core/Mycode/mpu6050.h
@@ -0,0 +1,24 @@
+//
+// Created by Administrator on 2026/4/7.
+//
+
+#ifndef AIRMOUSE_MPU6050_H
+#define AIRMOUSE_MPU6050_H
+
+#include "i2c.h"
+
+#define MPU6050_ADDR          (0x68 << 1)   // AD0接地时的地址
+#define MPU6050_PWR_MGMT_1    0x6B
+#define MPU6050_PWR_MGMT_2    0x6C
+#define MPU6050_SMPLRT_DIV    0x19
+#define MPU6050_CONFIG        0x1A
+#define MPU6050_GYRO_CONFIG   0x1B
+#define MPU6050_ACCEL_CONFIG  0x1C
+#define MPU6050_GYRO_XOUT_H   0x43    // 陀螺仪X轴高8位寄存器
+
+void MPU6050_WriteReg(uint8_t address,uint8_t data);
+
+void MPU6050_Init(void);
+void MPU6050_GetGyroDPS(float *wx, float *wy, float *wz);
+
+#endif //AIRMOUSE_MPU6050_H
